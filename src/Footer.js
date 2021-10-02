eec163e90f569988ba91de5cc1e870b4e49595ed
Omkar Waghchaure <waghchaureomkar@gmail.com>
2021-10-02 20:53:55 +0530

    final setup

---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+    return (
+        <div className="footer">
+            <h4 className="line"> Design By :  <a className="link" target="_blank" href="https://omkar-waghchaure.web.app/"> Omkar Waghchaure </a>  </h4>    
+        </div>
+    )
+}
+
+export default Footer;
